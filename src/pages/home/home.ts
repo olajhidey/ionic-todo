f4d6b76f589cecb4fb4f9efbfbc1c2dc26622abc
olajhidey <olajideawoyinka@gmail.com>
2017-07-10 11:22:29 +0100

    new commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,25 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  items : any[];
 
+  constructor(public navCtrl: NavController) {
+    this.items = []
+    for(let i = 0; i <= 10; i++) {
+       this.items.push({
+      text : 'Item'+i,
+      id: i 
+    })
+
+    }
+   
   }
+
+  itemSelected(item) {
+    alert(item.text)
+  }
+
+  
+
 
 }
